033a3543d24ece9a0e092ff757913a8b2f668167
Itallo Gravina <itallogravina@MacBook-Pro-de-Itallo.local>
2022-07-29 02:00:57 -0300

    confg chakra-ui
    Co-authored-by: lbrunofidelis <mailto:l.brunofidelis@gmail.com>
    Co-authored-by: nicaloribeiro <mailto:nicalo63@gmail.com>

---
+++ src/components/Login/index.tsx
@@ -0,0 +1,3 @@
+const Login = () => <h1>main</h1>
+
+export default Login
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-
-import GlobalStyles from 'styles/global'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -17,8 +16,9 @@ function App({ Component, pageProps }: AppProps) {
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
         />
       </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </>
   )
 }
